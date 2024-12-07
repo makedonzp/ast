3f1c4f075284ea106673ca8c37cf3a5b0a77239d
makedonzp <freefilosof@gmail.com>
2024-12-08 00:47:18 +0300

    mob adaptive

--- src/components/Rewards/Rewards.jsx
+++ src/components/Rewards/Rewards.jsx
@@ -23,11 +23,41 @@ export default function Rewards() {
         </Row>
         <Row className={styles.RewardsRowHands}>
           <Col className={styles.RewardsHandsWrapper}>
+            <div className={styles.RewardsHandsSteps}></div>
             <div className={styles.RewardsHandsImage}>
               <p className={styles.RewardsHandsText}>
                 2. Поделиться <br /> сайтом с другом
               </p>
             </div>
+          </Col>
+        </Row>
+        <Row className={styles.RewardsRowPaperNote}>
+          <Col className={styles.RewardsPaperNoteWrapper}>
+            <div className={styles.RewardsPaperNoteImage}>
+              <p className={styles.RewardsPaperNoteText}>
+                3. Оставить <br /> отзыв на книгу
+              </p>
+            </div>
+            <div className={styles.RewardsPaperNoteSteps}></div>
+          </Col>
+        </Row>
+        <Row className={styles.RewardsRowPazzle}>
+          <Col className={styles.RewardsPazzleWrapper}>
+            <div className={styles.RewardsPazzleImage}>
+              <p className={styles.RewardsPazzleText}>
+                4. Выиграть <br />в мини-игру
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row className={styles.RewardsRowPrize}>
+          <Col className={styles.RewardsPrizeWrapper}>
+            <div className={styles.RewardsPrizeImage}>
+              <p className={styles.RewardsPrizeText}>
+                5. Получить <br /> приз
+              </p>
+            </div>
+            <div className={styles.RewardsPrizeSteps}></div>
           </Col>
         </Row>
       </Container>
